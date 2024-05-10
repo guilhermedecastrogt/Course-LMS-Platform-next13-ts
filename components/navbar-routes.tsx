eeb335268974fe68279f531ad39c1f72db4950e6
guilhermedecastrogt <guilhermecastro1000@gmail.com>
2024-05-10 19:53:03 -0300

    create page and teacher admin

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,7 +1,11 @@
 "use client"
 
-import {UserButton} from "@clerk/nextjs";
-import {usePathname, useRouter} from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 export const NavbarRoutes = () => {
     const pathname = usePathname();
@@ -12,7 +16,23 @@ export const NavbarRoutes = () => {
     
     return (
         <div className="flex gap-x-2 ml-auto">
-            <UserButton />
+            {isTeacherPage || isPlayerPage ? (
+                <Link href="/">
+                    <Button size="sm" variant="ghost">
+                        <LogOut className="h-4 w-4 mr-2" />
+                        Exit
+                    </Button>
+                </Link>
+            ) : (
+                <Link href="/teacher/courses">
+                    <Button size="sm" variant="ghost">
+                        Teacher mode
+                    </Button>
+                </Link>
+            )}
+            <UserButton 
+                afterSignOutUrl="/"
+            />
         </div>
     )
 }
