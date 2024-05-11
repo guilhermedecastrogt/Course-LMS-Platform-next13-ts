31cb1218c9dc7ca68f861d2b229c6c726fcef7d3
guilhermedecastrogt <guilhermecastro1000@gmail.com>
2024-05-11 17:44:00 -0300

    create course frontend

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -9,10 +9,13 @@ import {
     FormDescription,
     FormField,
     FormLabel,
-    FormMessage
+    FormMessage,
+    FormItem
 } from "@/components/ui/form";
 import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
+import Link from "next/link";
+import {Button} from "@/components/ui/button";
 
 const formSchema =z.object({
     title: z.string().min(1, {
@@ -42,6 +45,50 @@ const CreatePage = () => {
             <p className="text-sm text-slate-600">
                 What would your like name your course? Don't worry, you can change this later.
             </p>
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-8 mt-8"
+                >
+                    <FormField 
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>
+                                    Course Title
+                                </FormLabel>
+                                <FormControl>
+                                    <input
+                                        disabled={isSubmitting}
+                                        placeholder="e.g. 'Advanced Web Development'" {...field} 
+                                    />
+                                </FormControl>
+                                <FormDescription>
+                                    What will you trach in this course?
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                            )}
+                    />
+                    <div className="flex items-center gap-x-2">
+                        <Link href="/">
+                            <Button
+                                type="button"
+                                variant="ghost"
+                            >
+                                Cancel
+                            </Button>
+                        </Link>
+                        <Button
+                            type="submit"
+                            disabled={!isValid || isSubmitting}
+                        >
+                            Continue
+                        </Button>
+                    </div>
+                </form>
+            </Form>
         </div>
     )
 }
