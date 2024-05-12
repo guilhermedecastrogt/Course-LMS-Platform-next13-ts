6d3bb709cac4e8ea3aaffca4cd0b9947d3298ba5
guilhermedecastrogt <guilhermecastro1000@gmail.com>
2024-05-11 22:54:25 -0300

    axios

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -12,10 +12,11 @@ import {
     FormMessage,
     FormItem
 } from "@/components/ui/form";
-import {useForm} from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 const formSchema =z.object({
     title: z.string().min(1, {
@@ -24,6 +25,7 @@ const formSchema =z.object({
 })
 
 const CreatePage = () => {
+    const router = useRouter();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -33,62 +35,69 @@ const CreatePage = () => {
     
     const { isSubmitting, isValid } = form.formState;
     
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try{
+            const response = await axios.post("/api/courses", values);
+            
+        } catch (ex) {
+            console.log("SOmething went wrong")
+        }
     }
     
     return (
         <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
-            <h1 className="text-2xl">
-                Name your courses
-            </h1>
-            <p className="text-sm text-slate-600">
-                What would your like name your course? Don't worry, you can change this later.
-            </p>
-            <Form {...form}>
-                <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="space-y-8 mt-8"
-                >
-                    <FormField 
-                        control={form.control}
-                        name="title"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    Course Title
-                                </FormLabel>
-                                <FormControl>
-                                    <input
-                                        disabled={isSubmitting}
-                                        placeholder="e.g. 'Advanced Web Development'" {...field} 
-                                    />
-                                </FormControl>
-                                <FormDescription>
-                                    What will you trach in this course?
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
+            <div>
+                <h1 className="text-2xl">
+                    Name your courses
+                </h1>
+                <p className="text-sm text-slate-600">
+                    What would your like name your course? Don't worry, you can change this later.
+                </p>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-8 mt-8"
+                    >
+                        <FormField
+                            control={form.control}
+                            name="title"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Course Title
+                                    </FormLabel>
+                                    <FormControl>
+                                        <input
+                                            disabled={isSubmitting}
+                                            placeholder="e.g. 'Advanced Web Development'" {...field}
+                                        />
+                                    </FormControl>
+                                    <FormDescription>
+                                        What will you trach in this course?
+                                    </FormDescription>
+                                    <FormMessage/>
+                                </FormItem>
                             )}
-                    />
-                    <div className="flex items-center gap-x-2">
-                        <Link href="/">
+                        />
+                        <div className="flex items-center gap-x-2">
+                            <Link href="/">
+                                <Button
+                                    type="button"
+                                    variant="ghost"
+                                >
+                                    Cancel
+                                </Button>
+                            </Link>
                             <Button
-                                type="button"
-                                variant="ghost"
+                                type="submit"
+                                disabled={!isValid || isSubmitting}
                             >
-                                Cancel
+                                Continue
                             </Button>
-                        </Link>
-                        <Button
-                            type="submit"
-                            disabled={!isValid || isSubmitting}
-                        >
-                            Continue
-                        </Button>
-                    </div>
-                </form>
-            </Form>
+                        </div>
+                    </form>
+                </Form>
+            </div>
         </div>
     )
 }
