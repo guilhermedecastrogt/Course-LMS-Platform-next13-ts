bc8ef5c680059e731a2694cb9a8b07b347c60680
guilhermedecastrogt <guilhermecastro1000@gmail.com>
2024-05-20 20:32:38 -0300

    uploadthings and edit description

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -6,4 +6,5 @@ import {
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
 export const UploadButton = generateUploadButton<OurFileRouter>();
+
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
